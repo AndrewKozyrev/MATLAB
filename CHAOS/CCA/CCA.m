e5664e8f8c6a44cea61cc9cdccaacbf2f3394c48
AndrewKozyrev <andrey_kozyrev_001@mail.ru>
2020-05-11 21:54:04 +0300

    Update CCA.m

--- CHAOS/CCA/CCA.m
+++ CHAOS/CCA/CCA.m
@@ -1,6 +1,11 @@
 classdef CCA
-    %UNTITLED4 Summary of this class goes here
-    %   Detailed explanation goes here
+    %CCA class describes chaotic cyclic attractor
+    %   methods: 
+    % getSequence() :  returns [x y] sequence
+    % groupZones() : returns groups of zones
+    % drawBorders() : void, draws borders around zones
+    % displayPortrait() : displays given zones: 'one' - particular zone, 'all' - all zones
+    % identify() : returns the number of zone given point belongs to
     
     properties
         a
@@ -13,70 +18,129 @@ classdef CCA
     end
     
     methods
-        function obj = CCA(in_a, in_b, in_x1, in_x2, in_period)
+        function self = CCA(in_a, in_b, in_x1, in_x2, in_period)
             %CCA Construct an instance of this class
             %   Detailed explanation goes here
-            obj.a = in_a;
-            obj.b = in_b;
-            obj.x1 = in_x1;
-            obj.x2 = in_x2;
-            obj.period = in_period;
-            obj.transient_count = 10^7;
-            obj.sequence_length = 10^6;
+            self.a = in_a;
+            self.b = in_b;
+            self.x1 = in_x1;
+            self.x2 = in_x2;
+            self.period = in_period;
+            self.transient_count = 10^4;
+            self.sequence_length = 10^5;
         end
         
-        function drawBorders(obj)
-            %drawBorders draws grid alike borders over chaotic zones on the
+        function drawBorders(self, choice)
+            %drawBorders draws grid like borders over chaotic zones on the
             %phase plane
             %   Depending on period, this function iterates through all
             %   chaotic zones and plots borders for each
-            n = obj.period;
-            [x, y] = obj.getSequence();
-            for i=1:n
-                left = min(x(i:n:end-1)); 
-                right = max(x(i:n:end-1)); 
-                bottom = min(x(i+1:n:end)); 
-                top = max(x(i+1:n:end));
+            dict = self.groupZones();
+            if isa(choice, 'double') && 0 < choice && choice <= self.period
+                temp    = dict(fix(choice));   %in case somebody throws me a floating value:o)
+                left    = min(temp(1,:)); 
+                right   = max(temp(1,:)); 
+                bottom  = min(temp(2,:)); 
+                top     = max(temp(2,:));
                 hold on
-                plot([left left], [bottom top], 'r')
-                plot([right right], [bottom top], 'r')
-                plot([left right], [bottom bottom], 'r')
-                plot([left right], [top top], 'r')
+                plot([left left], [bottom top], '--r')
+                plot([right right], [bottom top], '--r')
+                plot([left right], [bottom bottom], '--r')
+                plot([left right], [top top], '--r')
+                ylim([-1 1]);
+                xlim([-1 1]);
+            elseif ( isa(choice, 'char') || isa(choice, 'string') ) && (choice == "all")
+                for i=1:1:self.period
+                    temp    = dict(i);   %in case somebody throws me a floating value:o)
+                    left    = min(temp(1,:)); 
+                    right   = max(temp(1,:)); 
+                    bottom  = min(temp(2,:)); 
+                    top     = max(temp(2,:));
+                    hold on
+                    plot([left left], [bottom top], '--r')
+                    plot([right right], [bottom top], '--r')
+                    plot([left right], [bottom bottom], '--r')
+                    plot([left right], [top top], '--r')
+                    ylim([-1 1]);
+                    xlim([-1 1]);
+                end
+                ylim([-1 1]);
+                xlim([-1 1]);
+                hold off;
+            else
+                error("The argument '%s' is not valid", string(choice));
             end
         end
         
-        function mapping = groupZones(obj)
-            n = obj.period;
-            [x, y] = obj.getSequence();
+        function mapping = groupZones(self)
+            n = self.period;
+            [x, ~] = self.getSequence();
             mapping = containers.Map('KeyType','uint64','ValueType','any');
             for i=1:n
-                mapping(i-1) = [x(i:n:end-1); x(i+1:n:end)];
+                mapping(i) = [x(i:n:end-1); x(i+1:n:end)];
             end
         end
         
-        function [x, y] = getSequence(obj)
+        function [x, y] = getSequence(self)
             %retrieves a sequence from chaotic system of equations
-            M = obj.transient_count;
-            N = obj.sequence_length;
-            y = obj.a*obj.x2 + obj.b*obj.x1;
+            
+            % declare local variables
+            M = self.transient_count;
+            N = self.sequence_length;
+            temp_x1 = self.x1;
+            temp_x2 = self.x2;
+            temp_a = self.a;
+            temp_b = self.b;
+            y = temp_a*temp_x2 + temp_b*temp_x1;
+            % end of local variables declaration
+            
             % transient decay process
             for i=1:M
                 x3 = sin( pi*y );
-                obj.x1 = obj.x2;
-                obj.x2 = x3;
-                y = obj.a*obj.x2 + obj.b*obj.x1;
+                temp_x1 = temp_x2;
+                temp_x2 = x3;
+                y = temp_a*temp_x2 + temp_b*temp_x1;
             end
 
             clear x y
+            x = zeros(1, N);
+            y = zeros(1, N);
             % recording cycle
             for j=1:N
-                y(j) = obj.a*obj.x2 + obj.b*obj.x1;
-                x3 = sin( pi*y(j) );
-                obj.x1 = obj.x2;
-                obj.x2 = x3;
-                x(j) = x3;
+                y(j)  = temp_a*temp_x2 + temp_b*temp_x1;
+                x3    = sin( pi*y(j) );
+                temp_x1    = temp_x2;
+                temp_x2    = x3;
+                x(j)  = x3;
             end
 
+        end
+        
+        function displayPortrait(self, choice)
+            dict = self.groupZones();
+            if isa(choice, 'double') && 0 < choice && choice <= self.period
+                temp = dict(fix(choice));   %in case somebody throws me a floating value:o)
+                plot(temp(1, :), temp(2, :), '.', 'markersize', 3, 'color', 'b');
+                ylim([-1 1]);
+                xlim([-1 1]);
+            elseif ( isa(choice, 'char') || isa(choice, 'string') ) && (choice == "all")
+                for i=1:1:self.period
+                    temp = dict(i);
+                    plot(temp(1, :), temp(2, :), '.', 'markersize', 3, 'color', 'b');
+                    hold on;
+                end
+                ylim([-1 1]);
+                xlim([-1 1]);
+                hold off;
+            else
+                error("The argument '%s' is not valid", string(choice));
+            end
+        end
+        
+        function zone = indentify(self, x, y)
+            % This function should return the zone, to which the given
+            % point belongs. If the point does not belong to any zone, then
+            % just return -1
         end
     end
 end
