20fbe76bbe7b985bba079f9cf38f567af1eafb20
AndrewKozyrev <andrey_kozyrev_001@mail.ru>
2020-05-14 21:42:34 +0300

    Update CCA.m
    - Changed access to some attributes
    - Added attribute seed_x1
    - Added attribute seed_x2
    - Created transient function
    - Added getOrder function
    - Began to implement encode function

--- CHAOS/CCA/CCA.m
+++ CHAOS/CCA/CCA.m
@@ -7,14 +7,19 @@ classdef CCA < handle
     % displayPortrait() : displays given zones: 'one' - particular zone, 'all' - all zones
     % identify() : returns the number of zone given point belongs to
     
-    properties
+    properties (SetAccess = immutable)
         a
         b
+        period
+        seed_x1
+        seed_x2
+    end
+    properties (SetAccess = public)
         x1
         x2
-        period
-        transient_count
-        sequence_length
+        transient_count = 10^4;
+        sequence_length = 10^5;
+        initialized = false;
     end
     
     methods
@@ -23,11 +28,9 @@ classdef CCA < handle
             %   Initialize all possible parameters
             self.a = in_a;
             self.b = in_b;
-            self.x1 = in_x1;
-            self.x2 = in_x2;
+            self.seed_x1 = in_x1;       self.x1 = in_x1;
+            self.seed_x2 = in_x2;       self.x2 = in_x2;
             self.period = in_period;
-            self.transient_count = 10^4;
-            self.sequence_length = 10^5;
             if size(varargin, 2) > 0
                 self.sequence_length = varargin{1};
             end
@@ -35,8 +38,9 @@ classdef CCA < handle
                 self.transient_count = varargin{2};
             end
             if size(varargin, 2) > 2
-                error("This function doesn't accept so much arguments.");
+                error("Constructor doesn't accept so much arguments.");
             end
+            self.transient(self.transient_count, true);
         end
         
         function drawBorders(self, varargin)
@@ -44,7 +48,7 @@ classdef CCA < handle
             %phase plane
             %   Depending on period, this function iterates through all
             %   chaotic zones and plots borders for each
-            dict = self.groupZones();
+            dict = self.groupZones(true);
             if size(varargin, 2) == 0
                 for i=1:1:self.period
                     temp    = dict(i);   %in case somebody throws me a floating value:o)
@@ -75,6 +79,7 @@ classdef CCA < handle
                 plot([left right], [top top], '--r')
                 ylim([-1 1]);
                 xlim([-1 1]);
+                hold off
             elseif size(varargin, 2) > 1 && size(varargin, 2) < self.period
                 for i=1:1:size(varargin, 2)
                     temp = dict(varargin{i});
@@ -96,24 +101,24 @@ classdef CCA < handle
             end
         end
         
-        function mapping = groupZones(self)
+        function mapping = groupZones(self, varargin)
             n = self.period;
-            [x, ~] = self.getSequence();
+            if size(varargin, 2) == 1
+                [x, ~] = self.getSequence(varargin{1});
+            else
+                [x, ~] = self.getSequence();
+            end
             mapping = containers.Map('KeyType','uint64','ValueType','any');
             for i=1:n
                 mapping(i) = [x(i:n:end-1); x(i+1:n:end)];
             end
         end
         
-        function [self, x, y] = getSequence(self, varargin)
+        function [x, y] = getSequence(self, varargin)
             %retrieves a sequence from chaotic system of equations
             
             % declare local variables
-            if size(varargin, 2) == 1 && isa(varargin{1}, 'double')
-                M = varargin{1};
-            else
-                M = self.transient_count;
-            end
+            M = self.transient_count;
             N = self.sequence_length;
             temp_x1 = self.x1;
             temp_x2 = self.x2;
@@ -121,15 +126,14 @@ classdef CCA < handle
             temp_b = self.b;
             y = temp_a*temp_x2 + temp_b*temp_x1;
             % end of local variables declaration
-            
-            % transient decay process
-            for i=1:M
-                x3 = sin( pi*y );
-                temp_x1 = temp_x2;
-                temp_x2 = x3;
-                y = temp_a*temp_x2 + temp_b*temp_x1;
+                 
+            % transient decay process                       
+            if self.initialized == false
+                % drop first M unwanted points
+                self.transient(M);
+                self.initialized = true;
             end
-
+            
             clear x y
             x = zeros(1, N);
             y = zeros(1, N);
@@ -141,13 +145,36 @@ classdef CCA < handle
                 temp_x2    = x3;
                 x(j)  = x3;
             end
-            % update the initial condition variables
-            self.CCA() = x(end-1);
-            self.x2 = x(end);
+            % update the initial condition variables if asked
+            if size(varargin, 2) == 1 && isa(varargin{1}, 'logical') && varargin{1}
+                self.x1 = x(end-1);
+                self.x2 = x(end);
+            end
+        end
+        
+        function [new_x1, new_x2] = transient(self, M, varargin)
+            % local variables again
+            temp_x1 = self.x1;
+            temp_x2 = self.x2;
+            temp_a = self.a;
+            temp_b = self.b;
+            y = temp_a*temp_x2 + temp_b*temp_x1;            
+            for i=1:M
+                x3 = sin( pi*y );
+                temp_x1 = temp_x2;
+                temp_x2 = x3;
+                y = temp_a*temp_x2 + temp_b*temp_x1;
+            end
+            new_x1 = temp_x1;
+            new_x2 = temp_x2;
+            if size(varargin, 2) == 1 && isa(varargin{1}, 'logical') && varargin{1}
+                self.x1 = new_x1;
+                self.x2 = new_x2;
+            end
         end
         
         function displayPortrait(self, varargin)
-            dict = self.groupZones();
+            dict = self.groupZones(true);
             if size(varargin, 2) == 0
                 for i=1:1:self.period
                     temp = dict(i);
@@ -163,6 +190,7 @@ classdef CCA < handle
                 plot(temp(1, :), temp(2, :), '.', 'markersize', 3, 'color', 'b');
                 ylim([-1 1]);
                 xlim([-1 1]);
+                hold off
             elseif size(varargin, 2) > 1 && size(varargin, 2) < self.period
                 for i=1:1:size(varargin, 2)
                     temp = dict(varargin{i});
@@ -181,8 +209,8 @@ classdef CCA < handle
             % This function should return the zone, to which the given
             % point belongs. If the point does not belong to any zone, then
             % just return -1
-            dict = self.groupZones();
-            [temp, ~] = self.getSequence(0);
+            dict = self.groupZones(false);      % don't update initial conditions
+            [temp, ~] = self.getSequence(true);      %update initial conditions
             [k, dist] = dsearchn([temp(1:end-1); temp(2:end)]',  point);
 
             if dist > 10^-3
@@ -199,7 +227,61 @@ classdef CCA < handle
             end
         end
         
+        function [key, start_of_hopping] = getOrder(self, varargin)
+            load('zone_clusters.mat', 'data');
+            temp_x1 = self.x1; 
+            temp_x2 = self.x2;
+            treshold_1 = 10^-6; treshold_2 = 10^-3;
+            if size(varargin, 2) > 0 && isa(varargin{1}, 'double')
+                temp_x1 = varargin{1}(1);
+                temp_x2 = varargin{1}(2);
+            end
+            if size(varargin, 2) > 2 && isa(varargin{2}, 'double') && isa(varargin{3}, 'double')
+                treshold_1 = varargin{2};
+                treshold_2 = varargin{3};
+            end
+            temp_a = self.a;
+            temp_b = self.b;
+            y = temp_a*temp_x2 + temp_b*temp_x1;
+            
+            % This loop is meant to allign the pivot to CZ0
+            for i=1:10^7
+                temp_x3 = sin( pi*y );
+                temp_x1 = temp_x2;
+                temp_x2 = temp_x3;
+                y = temp_a*temp_x2 + temp_b*temp_x1;
+                [~, dist] = dsearchn(data(1)',  [temp_x1 temp_x2]);     % allign the start of cycle to the CZ0
+                if dist <= treshold_1
+                    start_of_hopping = [temp_x1 temp_x2];           % this point belongs to CZ0
+                    key = string(1);
+                    break
+                end
+            end
+            
+            for i=1:1:8
+                y = temp_a*temp_x2 + temp_b*temp_x1;
+                temp_x3 = sin( pi*y );
+                temp_x1 = temp_x2;
+                temp_x2 = temp_x3;
+                % this loop checks where the next point belongs, i.e. the
+                % corresponding chaotic zone CZ(k)
+                for k=1:1:self.period                       %iterate through all zones, # of zones = period
+                    [~, dist] = dsearchn(data(k)',  [temp_x1 temp_x2]);
+                    if dist <= treshold_2
+                        key = key + string(k);          %append the next chaotic zone index
+                        break
+                    end
+                end
+            end
+        end
+        
+        function encode(~, varargin)
+            %encode() encodes a message with this non-linear model
+            %   encode(message) takes message and returns cypther
+            decimal_input = bi2de(varargin{1});
+            base_transform = dec2base(decimal_input, 8);
+        end
+        
     end
-    
 end
 
