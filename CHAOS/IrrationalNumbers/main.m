fbe1ff8217edfb4d4fe4d470e8595e2548a99f96
AndrewKozyrev <andrey_kozyrev_001@mail.ru>
2020-07-01 20:48:17 +0300

    Update main.m

--- CHAOS/IrrationalNumbers/main.m
+++ CHAOS/IrrationalNumbers/main.m
@@ -1,10 +1,10 @@
 %% Our noise
 cd 'C:\Users\hitma\OneDrive\ROOT\Files\Job\Projects\MATLAB\CHAOS\IrrationalNumbers';
-clc, clear, format long
+clear, format long
 % variables
-n = 100;
-dig = 32;
+n = 1000000;
 tic
-x = sym(2^sym(1/2)-1);
-s = Dyadic.getBinary(x, n, 'approximate', dig)       % irrational numbers noise
+x = sym(3^sym(1/2)-2^sym(1/2));
+l = 4;
+s = Dyadic.getBinary(x, n, 'approximate')       % irrational numbers noise
 cypher_time = toc
